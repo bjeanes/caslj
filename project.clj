23d754ba8ce1ee8fcb94de51fc1b1e7b3f33c5a8
Bo Jeanes <me@bjeanes.com>
2012-08-10 15:18:06 -0500

    Move ring-mock to dev profile

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject caslj "0.1.0-SNAPSHOT"
-  :description "CAS server pronounced 'Castle'"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
+            :description "CAS server pronounced 'Castle'"
+            :url "http://example.com/FIXME"
+            :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring "1.1.1"]
-                 [compojure "1.1.1"]
-                 [trammel "0.7.0"] ]
-  :main caslj.main)
+            :dependencies [[org.clojure/clojure "1.4.0"]
+                           [ring "1.1.1"]
+                           [compojure "1.1.1"]
+                           [trammel "0.7.0"]]
+            :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}}
+            :main caslj.main)
